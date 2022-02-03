366c96e64f07dec7bb3708cee01efbb6d7bf2562
lbageant17 <erb32@cornell.edu>
2022-02-03 09:09:21 -0500

    Reorganizing scripts, further exploratory analysis

---
+++ scripts/MASTER.R
@@ -0,0 +1,26 @@
+## Liz Bageant
+## Master file for fisher mental health project
+
+
+## PACKAGES
+#install.packages("tidyverse")
+#install.packages("skimr")
+#install.packages("ggpubr")
+library(ggpubr) 
+library(tidyverse) 
+library(skimr)
+
+
+
+## FILE PATHS
+
+projectFolder  <- "~/github/fisher_mental_health"
+data <- file.path(projectFolder, "data/csv")
+
+
+## Related scripts, in order
+
+# data_prep.Rmd
+# explore_demographics
+# explore_access
+# explore_mh
